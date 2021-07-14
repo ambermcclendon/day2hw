5ded1a5208ca2e14c195acc8095b30fd854bde12
Amber <Amberrmcclendon@gmail.com>
2021-07-14 16:32:50 -0400

    push hw

---
+++ app.js
@@ -0,0 +1,16 @@
+function addSum(x,y){
+    return x + y;
+}
+console.log(addSum(3,5));
+function subtractSum(x,y){
+    return x - y;
+}
+console.log(subtractSum(3,5))
+function multiplySum(x,y){
+    return x * y;
+}
+console.log(multiplySum(3,5))
+function divideSum(x,y){
+    return x / y;
+}
+console.log(multiplySum(3,5))
